9ac16b5c58427cab3b2852f146f3fc4a91acbd0f
Patrick TrollipI <ptrollip@gmail.com>
2020-02-19 02:57:42 +0000

    Tested products and services models and then corrected the exception errors

--- products/models.py
+++ products/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Product(models.Model):
-    image = models.ImageField(upload_to='media/images')
+    image = models.ImageField(upload_to='images')
     name = models.CharField(max_length=200, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=7, decimal_places='2')
+    price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
         return self.name
--- services/models.py
+++ services/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=200, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=7, decimal_places='2')
+    price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
         return self.name
