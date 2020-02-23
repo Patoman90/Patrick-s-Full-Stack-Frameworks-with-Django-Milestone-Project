7523abe1ff6182b73f449c539b5a99b336bdd979
Patrick TrollipI <ptrollip@gmail.com>
2020-02-23 22:54:35 +0000

    Updated the base.html file so that it is extended to the other html files

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+"""This Model creates a model called Reviews that gives data fields that include text and images"""
+
+
+class Reviews(models.Model):
+    Product = models.CharField(max_length=200, default='')
+    Review = models.TextField()
+
+    def __str__(self):
+        return self.name
