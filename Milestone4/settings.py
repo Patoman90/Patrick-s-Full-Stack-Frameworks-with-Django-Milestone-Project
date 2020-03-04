ed06df6d506bb0e182595b6befd932b49ae1d2c7
Patrick TrollipI <ptrollip@gmail.com>
2020-03-04 12:36:35 +0000

    updated allowed hosts insettings.py

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','patricks4thmilestoneproject.herokuapp.com']
 
 
 # Application definition
