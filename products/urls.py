60565757d74189106d7af16073bc5f8dc4020d84
Patrick TrollipI <ptrollip@gmail.com>
2020-03-06 10:12:54 +0000

    Updated reviews.html and added a extra field

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from .views import show_all_products
+from reviews import urls as urls_reviews 
 
 
 urlpatterns = [
     url(r'^$', show_all_products, name='products'),
+    url(r'^reviews/', include(urls_reviews))
 ]
