e5f8f7866209dd7454960ae3a64d08f6d959c6df
Patrick Trollip <ptrollip@gmail.com>
2020-03-08 10:39:32 +0000

    saved changes to keys

--- env.py
+++ env.py
@@ -4,6 +4,6 @@ os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_JXiYq88GId61oVx2xYGMadex00U
 os.environ.setdefault("STRIPE_SECRET", "sk_test_2nfkE8dGGOPKtLSmEakpIzJe00nclyBvP2")
 os.environ.setdefault("DATABASE_URL", "postgres://ahrtbuklahwish:b360de9b1dc8a08a13330369601da3ee85a1868af9df26bd41918dab42bf4713@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d417npd6nqbqeg")
 os.environ.setdefault("SECRET_KEY", "zj8z@wd@vkb@)@fuj_*s(kgfi#v5^ny5xbf-i81+77@63&9nu9")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAIJRNRE5LCFPJVIDA")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAIWZYEEM6XQP5WC6Q")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
 os.environ.setdefault("AWS_STORAGE_BUCKET_NAME", "patricks-milestone4")
