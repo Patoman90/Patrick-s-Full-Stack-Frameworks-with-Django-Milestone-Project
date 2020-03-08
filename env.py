abc2fad9c14a1b869da4c12436f1a5497a6d5c71
Patrick Trollip <ptrollip@gmail.com>
2020-03-08 14:48:55 +0000

    updated env.py

--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_JXiYq88GId61oVx2xYGMadex00UuobHGmt")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_2nfkE8dGGOPKtLSmEakpIzJe00nclyBvP2")
-os.environ.setdefault("DATABASE_URL", "postgres://ahrtbuklahwish:b360de9b1dc8a08a13330369601da3ee85a1868af9df26bd41918dab42bf4713@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d417npd6nqbqeg")
-os.environ.setdefault("SECRET_KEY", "z$#4r@aiceh+srvtmupg)ya#g&p5hu5c^pj!mz=79%h-%na*bu")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAJT7QY7DEKIUC7DCA")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
+os.environ.setdefault("STRIPE_PUBLISHABLE", os.getenv("STRIPE_PUBLISHABLE"))
+os.environ.setdefault("STRIPE_SECRET", os.getenv("STRIPE_SECRET"))
+os.environ.setdefault("DATABASE_URL", os.getenv("DATABASE_URL"))
+os.environ.setdefault("SECRET_KEY", os.getenv("SECRET_KEY"))
+os.environ.setdefault("AWS_ACCESS_KEY_ID", os.getenv("AWS_ACCESS_KEY_ID"))
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", os.getenv("AWS_SECRET_ACCESS_KEY"))
 os.environ.setdefault("AWS_STORAGE_BUCKET_NAME", "patricks-milestone4")
