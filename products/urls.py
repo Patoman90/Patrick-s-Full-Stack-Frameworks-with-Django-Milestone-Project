aa4b9bac1474db66c8f085b6b7647aeda6d2942e
Patrick Trollip <ptrollip@gmail.com>
2020-07-20 11:05:35 +0000

    Updated the urls in the products app to fix the problem with only loading the products page

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,8 @@ from reviews import urls as urls_reviews
 from accounts.urls import quote, email
 
 urlpatterns = [
-    url(r'^$', show_all_products, name='products'),
-    url(r'^$', show_all_services, name='services'),
+    url(r'^product/$', show_all_products, name='products'),
+    url(r'^service/$', show_all_services, name='services'),
     url(r'^reviews/', include(urls_reviews)),
     url(r'^quote/$', quote, name='quote'),
     url(r'^email/$', email, name='email')
